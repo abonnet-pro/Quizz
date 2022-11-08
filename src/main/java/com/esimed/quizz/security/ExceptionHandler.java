66797e15423d967a6c66df56352e92f3a32ca843
abonnet <anthony.bonnet1@orange.fr>
2022-11-08 15:43:56 +0100

    Rename handler

--- src/main/java/com/esimed/quizz/security/ExceptionHandler.java
+++ src/main/java/com/esimed/quizz/security/ExceptionHandler.java
@@ -4,15 +4,14 @@ package com.esimed.quizz.security;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
-public class Handler {
+public class ExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handle(Exception ex, HttpServletRequest request, HttpServletResponse response) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
