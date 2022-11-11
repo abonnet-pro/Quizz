df5c5645cecd4b8454ac8e20ee6e8eaa3594f5fb
abonnet <anthony.bonnet1@orange.fr>
2022-11-11 18:21:41 +0100

    Correction securit

--- src/main/java/com/esimed/quizz/controllers/CategorieController.java
+++ src/main/java/com/esimed/quizz/controllers/CategorieController.java
@@ -5,10 +5,12 @@ import com.esimed.quizz.models.entities.Categorie;
 import com.esimed.quizz.models.mappers.CategorieMapper;
 import com.esimed.quizz.services.CategorieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.stream.Collectors;
 
--- src/main/java/com/esimed/quizz/security/SecurityConfiguration.java
+++ src/main/java/com/esimed/quizz/security/SecurityConfiguration.java
@@ -3,11 +3,13 @@ package com.esimed.quizz.security;
 import com.esimed.quizz.services.IUserService;
 import com.esimed.quizz.services.JwtService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -31,9 +33,10 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf()
-                .disable()
+                .csrf().disable()
+                .cors().disable()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/signup").permitAll()
                 .antMatchers("/signin").permitAll()
                 .anyRequest().authenticated()
